0018f9b038016a23f58e1c9966dd6bf8f9c2a783
MissRai13 <raisarina207@gmail.com>
2024-12-24 15:43:03 +0000

    Add files via upload

---
+++ week-3/person.js
@@ -0,0 +1,17 @@
+class student { 
+    constructor(name, age, email) { 
+    this.name = sarina; 
+    this.age = 20; 
+    this.email = raisarina207; 
+    } 
+     
+    getPersonInfo() { 
+    return 
+    Name: this.name 
+    Age: this.age 
+    Email: this.email 
+     
+    } 
+    } 
+     
+    module.exports = student;
